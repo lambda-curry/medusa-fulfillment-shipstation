6a2873446a3e6bbbdc69233d67bd3b2370a85559
Derek Wene <derek.wene@gmail.com>
2022-02-18 14:37:24 -0600

    chore: cleanup logs

--- src/api/webhook.ts
+++ src/api/webhook.ts
@@ -4,7 +4,7 @@ import { EventBusService } from '@medusajs/medusa/dist/services';
 export function webhook(req: any, res: Response) {
   const eventBus: EventBusService = req.scope.resolve('eventBusService');
   const logger = req.scope.resolve('logger');
-  logger.warn(`RECIEVED SHIPSTATION WEBHOOK: ${JSON.stringify(req.body)}`);
+  logger.info(`ShipStation webhook received: ${JSON.stringify(req.body)}`);
   eventBus.emit('shipstation.webhook', req.body);
   return res.status(200).json({ message: 'OK' });
 }
